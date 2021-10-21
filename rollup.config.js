3d0b57bd79514b8d00e3bccf71becaecb469b9d7
enatario <elaina.natario@gmail.com>
2021-10-21 14:03:11 -0400

    Update packages and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from "rollup-plugin-terser";
 import babel from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: "./src/js/app.js",
